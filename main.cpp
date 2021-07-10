795f385682866199a517034a791e5741b7218a96
Fidel_white <57117517+arcljq@users.noreply.github.com>
2021-07-10 22:56:18 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <list>
+
+using namespace std;
+
+#include "Stack.h"
+#include "Calculator.h"
+
+void InputAndCalculator(list<Calculator>& listCalculator);
+void Output(const list<Calculator>& listCalculator)
